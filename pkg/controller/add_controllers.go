60c81d9271fb19320b2afeaf4aab25e11806c6cf
Anton_Tarianyk <Anton_Tarianyk@epam.com>
2020-09-04 12:20:04 +0300

    [EPMDEDP-5536]: Create GitTag API; Push tag to repo
    Change-Id: I303c818ab5937173c83f637f1f8ac150f9278981

--- pkg/controller/add_controllers.go
+++ pkg/controller/add_controllers.go
@@ -1,19 +1,15 @@
 package controller
 
 import (
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/codebase"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/codebasebranch"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/gitserver"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/imagestreamtag"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/jirafixversion"
-	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/jiraserver"
+	"github.com/epmd-edp/codebase-operator/v2/pkg/controller/gittag"
 )
 
 func init() {
-	AddToManagerFuncs = append(AddToManagerFuncs, codebase.Add)
+	/*AddToManagerFuncs = append(AddToManagerFuncs, codebase.Add)
 	AddToManagerFuncs = append(AddToManagerFuncs, codebasebranch.Add)
 	AddToManagerFuncs = append(AddToManagerFuncs, gitserver.Add)
 	AddToManagerFuncs = append(AddToManagerFuncs, jirafixversion.Add)
 	AddToManagerFuncs = append(AddToManagerFuncs, jiraserver.Add)
-	AddToManagerFuncs = append(AddToManagerFuncs, imagestreamtag.Add)
+	AddToManagerFuncs = append(AddToManagerFuncs, imagestreamtag.Add)*/
+	AddToManagerFuncs = append(AddToManagerFuncs, gittag.Add)
 }
